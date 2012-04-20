6b7249b3476ffb5ebff25f5fee40603f482860f7
Jonan Scheffler <jonanscheffler@gmail.com>
2012-04-19 21:22:41 -0400

    Omnium rerum principia parva sunt.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hungrypress::Application.config.secret_token = '2afca93157f8a37cbbaeec6e4c1370759184be6825e8018d902019dda3d8225a8273294e6f4feecfbaaae497897e92f779b259636811701e852416cff3172939'
