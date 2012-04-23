a98049a8e7cce96e0c9ae22d2f50fc2c954a0c7a
Jonan Scheffler <jonanscheffler@gmail.com>
2012-04-23 16:23:04 -0400

    Add cancan and users#index authorization.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # Please keep your filthy paws off of this file unless you have a great reason.
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,10 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
 
+
   def index
     @users = User.all
+    authorize! :index, @users
   end
 
   def show
